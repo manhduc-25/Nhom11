1c5215b58d1b285d2345b93e47af4cbe397bc5fe
NDK1074 <khiemnd2972@ut.edu.vn>
2025-07-12 07:50:52 +0700

    Update logout.php

--- flower_gift_shop/admin/logout.php
+++ flower_gift_shop/admin/logout.php
@@ -1,5 +1,11 @@
 <?php
 session_start();
+
+// Xoá toàn bộ session (admin)
+session_unset();
 session_destroy();
+
+// Chuyển hướng về trang đăng nhập admin
 header("Location: login.php");
 exit;
+?>
